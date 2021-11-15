6b28ded67217ea00c85f4af8d5c7b5f4135bfda4
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2021-11-15 01:41:33 -0300

    creando footer y animacion del mismo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type {AppProps} from "next/app";
+import {Fragment} from "react";
+
+import Footer from "../components/Layout/Footer";
 
 function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <nav>Hola Soy un navbar</nav>
+      <Component {...pageProps} />
+      <Footer />
+    </Fragment>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from "next";
 import Head from "next/head";
 
 import GifSearchComponent from "../components/GifSearch";
+import Footer from "../components/Layout/Footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
