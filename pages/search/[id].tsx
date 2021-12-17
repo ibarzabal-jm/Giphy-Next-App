85720aee750e0efac1a8d79f95dd066a3fdd7894
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2021-12-17 18:13:00 -0300

    No more gifs infinite

--- pages/search/[id].tsx
+++ pages/search/[id].tsx
@@ -24,11 +24,13 @@ interface Props {
 
 const SearchPage: NextPage<Props> = ({gifs, keyword}) => {
   const [gifsArray, setGifsArray] = useState([...gifs]);
+  const [noMoreGifs, setNoMoreGifs] = useState(false);
   const externalRef = useRef(null);
   const {gifs: newgifs, nextPage, status} = useFetchGif({keyword, immediate: false});
   const {isNearScreen} = useNearScreen({
     distance: "100px",
     externalRef: status === "pending" ? null : externalRef,
+    once: noMoreGifs,
   });
 
   useEffect(() => {
@@ -36,6 +38,7 @@ const SearchPage: NextPage<Props> = ({gifs, keyword}) => {
   }, [isNearScreen]);
 
   useEffect(() => {
+    newgifs.length === 0 && setNoMoreGifs(true);
     setGifsArray((prev) => [...prev, ...newgifs]);
   }, [newgifs]);
 
@@ -45,6 +48,7 @@ const SearchPage: NextPage<Props> = ({gifs, keyword}) => {
       <main>
         <ListOfGifs gifs={gifsArray} masonry={true} />
         {status === "pending" && <div style={{background: "red", height: "200px"}}>Cargandooo</div>}
+        {noMoreGifs && "Fin"}
       </main>
       <div ref={externalRef} />
     </section>
