de8e4fc7244b970ab389ce618a750d5fb0b226c0
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2022-01-05 01:42:07 -0300

    get trending gif

--- api/api.ts
+++ api/api.ts
@@ -7,4 +7,25 @@ export const api = {
       .then((res) => res.json())
       .then((response: ApiResponse) => response.data);
   },
+
+  getTrendingGif: async (limit = 10, offset = 0): Promise<Gif[]> => {
+    return await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=ePoEafBUZMXHRZ7n5TS0zC8ICn2wn0kC&limit=${limit}&offset=${offset}&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response: ApiResponse) => response.data);
+  },
+
+  getSingleGif: async (id: string): Promise<Gif> => {
+    return await fetch(`https://api.giphy.com/v1/gifs/${id}?api_key=ePoEafBUZMXHRZ7n5TS0zC8ICn2wn0kC&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response: ApiResponse) => response.data[0]);
+  },
+
+  getRandomGif: async (): Promise<Gif> => {
+    return await fetch(`https://api.giphy.com/v1/gifs/random?api_key=ePoEafBUZMXHRZ7n5TS0zC8ICn2wn0kC&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response: ApiResponse) => response.data[0]);
+  },
 };
