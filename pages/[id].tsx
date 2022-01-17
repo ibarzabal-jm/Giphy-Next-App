24a06f35265ca7b72ab68e39378560a4b1c768b4
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2022-01-17 02:28:42 -0300

    create gif landing page

---
+++ pages/[id].tsx
@@ -0,0 +1,27 @@
+import NextHead from "@components/Layout/NextHead";
+import {api} from "api/api";
+import {GetServerSideProps, NextPage} from "next/types";
+
+import {Gif} from "../types/ApiResponse";
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+  const id = params?.id as string;
+  const gif = await api.getSingleGif(id);
+
+  return {
+    props: {gif},
+  };
+};
+
+const GifLandingPage: NextPage<{gif: Gif}> = ({gif}) => {
+  const {id, images, title, username, bitly_gif_url} = gif;
+
+  return (
+    <div>
+      <NextHead title={title} />
+      <main className="container">{title}</main>
+    </div>
+  );
+};
+
+export default GifLandingPage;
