cc7f9da23296a36cefdab860d9b203622081232b
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2022-01-17 02:29:10 -0300

    convert gifcard to link

--- components/GifCard/GifCard.tsx
+++ components/GifCard/GifCard.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHeart} from "@fortawesome/free-solid-svg-icons";
 import {faHeart as faRegHeart} from "@fortawesome/free-regular-svg-icons";
+import Link from "next/link";
 
 import {Gif} from "../../types/ApiResponse";
 import {useFavs} from "../../hooks/useFavs";
@@ -47,50 +48,53 @@ const GifCard: React.FC<Props> = ({
     title,
     images: {original: imageMain},
     user,
+    id,
   } = image;
 
   const {isFavorite, toggleFav} = useFavs();
 
   return (
-    <div className={`${styles.card} ${styles[color]}`}>
-      <div className={styles.main_image}>
-        <button className={styles.favButton} onClick={() => toggleFav(image)}>
-          {isFavorite(image) ? (
-            <FontAwesomeIcon aria-label="Fav" color="#f00" icon={faHeart} size="lg" />
-          ) : (
-            <FontAwesomeIcon aria-label="NoFavorite" icon={faRegHeart} size="lg" />
-          )}
-        </button>
-        <Image
-          alt={title}
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(
-            shimmer(
-              widthProp ? widthProp : imageMain.width,
-              heightProp ? heightProp : imageMain.height,
-            ),
-          )}`}
-          height={heightProp ? heightProp : imageMain.height}
-          layout="responsive"
-          objectFit="cover"
-          placeholder="blur"
-          priority={priority}
-          src={imageMain.url}
-          unoptimized={true}
-          width={widthProp ? widthProp : imageMain.width}
-        />
-      </div>
-      {user && (
-        <div className={styles.user}>
-          <div className={styles.userimage}>
-            <Image alt={user.username} height={32} src={user?.avatar_url} width={32} />
-          </div>
-          <div className={styles.username}>
-            <p>{user.display_name}</p>
-            {user.is_verified && <CheckSvg />}
-          </div>
+    <Link href={`/${id}`}>
+      <a className={`${styles.card} ${styles[color]}`}>
+        <div className={styles.main_image}>
+          <button className={styles.favButton} onClick={() => toggleFav(image)}>
+            {isFavorite(image) ? (
+              <FontAwesomeIcon aria-label="Fav" color="#f00" icon={faHeart} size="lg" />
+            ) : (
+              <FontAwesomeIcon aria-label="NoFavorite" icon={faRegHeart} size="lg" />
+            )}
+          </button>
+          <Image
+            alt={title}
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimmer(
+                widthProp ? widthProp : imageMain.width,
+                heightProp ? heightProp : imageMain.height,
+              ),
+            )}`}
+            height={heightProp ? heightProp : imageMain.height}
+            layout="responsive"
+            objectFit="cover"
+            placeholder="blur"
+            priority={priority}
+            src={imageMain.url}
+            unoptimized={true}
+            width={widthProp ? widthProp : imageMain.width}
+          />
         </div>
-      )}
-    </div>
+        {user && (
+          <div className={styles.user}>
+            <div className={styles.userimage}>
+              <Image alt={user.username} height={32} src={user?.avatar_url} width={32} />
+            </div>
+            <div className={styles.username}>
+              <p>{user.display_name}</p>
+              {user.is_verified && <CheckSvg />}
+            </div>
+          </div>
+        )}
+      </a>
+    </Link>
   );
 };
 
