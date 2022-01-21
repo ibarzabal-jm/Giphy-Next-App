a3096376e05c6a893cb04721c9274fd8b9e3683c
Juanma Ibarzabal <ibarzabalsallesjm@gmail.com>
2022-01-20 23:05:33 -0300

    structure singlePage

--- api/api.ts
+++ api/api.ts
@@ -37,4 +37,28 @@ export const api = {
       .then((res) => res.json())
       .then((response) => response.data);
   },
+
+  autoComplete: async (keyword: string): Promise<string[]> => {
+    return await fetch(`https://api.giphy.com/v1/gifs/search/tags?api_key=${APIKEY}&q=${keyword}&limit=12&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response) => response.data);
+  },
+
+  getSearchSuggestions: async (keyword: string): Promise<string[]> => {
+    return await fetch(`https://api.giphy.com/v1/tags/related/${keyword}?api_key=${APIKEY}&limit=12&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response) => response.data)
+      .then((data) =>
+        data.map((item: {name: string; analytics_response_payload: string}) => item.name),
+      );
+  },
+
+  getRelatedGifs: async (id: string): Promise<Gif[]> => {
+    return await fetch(`https://api.giphy.com/v1/gifs/related?gif_id=${id}&api_key=${APIKEY}&limit=12&rating=g&lang=es
+    `)
+      .then((res) => res.json())
+      .then((response: ApiResponse) => response.data);
+  },
 };
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -3,6 +3,7 @@ import NextHead from "@components/Layout/NextHead";
 import TitleNeon from "@components/Layout/TitleNeon";
 import {api} from "api/api";
 import Image from "next/image";
+import ListOfGifs from "@components/ListOfGifs/ListOfGifs";
 
 import {Gif} from "../types/ApiResponse";
 
@@ -20,34 +21,74 @@ export const getServerSideProps: GetServerSideProps = async ({params}) => {
     };
   }
 
+  const [relatedGifs, suggestions] = await Promise.allSettled([
+    api.getRelatedGifs(gif.id),
+    api.getSearchSuggestions(gif.title),
+  ]);
+
   return {
     props: {
       gif,
+      relatedGifs: relatedGifs.status === "fulfilled" ? relatedGifs.value : [],
+      suggestions: suggestions.status === "fulfilled" ? suggestions.value : [],
     },
   };
 };
 
-const GifLandingPage: NextPage<{gif: Gif}> = ({gif}) => {
-  const {id, images, title, username} = gif;
+const GifLandingPage: NextPage<{
+  gif: Gif;
+  relatedGifs: Gif[];
+  suggestions: string[];
+}> = ({gif, relatedGifs, suggestions}) => {
+  const {images, title, user} = gif;
 
   return (
     <div>
       <NextHead title={title} />
-      <aside>
-        {gif.source}
-        {gif.source_post_url}
-        {gif.user?.avatar_url}
-      </aside>
-      <main className="container">
-        <TitleNeon color="#ffaaff" tag="h1" title={title} />
-        <Image
-          unoptimized
-          alt={title}
-          height={images.original.height}
-          src={images.original.webp}
-          width={images.original.width}
-        />
-      </main>
+      <div className="container">
+        <aside>
+          {user ? (
+            <a className="user" href={user.profile_url}>
+              <Image alt={user.username} height={50} src={user.avatar_url} width={50} />
+              <div>
+                @{user.username}
+                {user.is_verified && "verificado"}
+              </div>
+              <div>{user.description}</div>
+            </a>
+          ) : null}
+          <div>
+            <p>{gif.source_post_url}</p>
+            <p>Rating: {gif.rating}</p>
+            <p>Source:{gif.source}</p>
+            <p>Imported at : {gif.import_datetime}</p>
+          </div>
+        </aside>
+
+        <main>
+          <div>
+            <TitleNeon color="#ffaaff" size="48px" tag="h1" textTransform="none" title={title} />
+            <Image
+              unoptimized
+              alt={title}
+              height={images.original.height}
+              src={images.original.webp}
+              width={images.original.width}
+            />
+          </div>
+          <div className="suggestions">
+            {suggestions.map((suggest) => (
+              <a key={suggest} href={`/search/${suggest}`}>
+                #{suggest}
+              </a>
+            ))}
+          </div>
+          <div>
+            <TitleNeon size="32px" tag="h3" title="Related Gif" />
+            <ListOfGifs gifs={relatedGifs} masonry={false} />
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
